29aade760764a158fe69118a0cc268fc5c5284e0
CloudCampus <cloudcampus001@gmail.com>
2026-06-28 18:30:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
 import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    TanStackStartVite({
+    tsconfigPaths(),
+    tanstackStart({
       server: {
         preset: "vercel",
         entry: "src/server.ts",
       },
     }),
-    TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     tailwindcss(),
-    tsconfigPaths(),
   ],
 });
